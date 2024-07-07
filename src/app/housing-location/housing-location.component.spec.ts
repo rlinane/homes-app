0aa51df524e412fcfe4971b14c73d2bd72228024
rlinane <rlinane@gmail.com>
2024-07-07 12:54:34 -0400

    added housing location spec

--- src/app/housing-location/housing-location.component.spec.ts
+++ src/app/housing-location/housing-location.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HousingLocationComponent } from './housing-location.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import {HousingLocation} from '../housinglocation';
 
 describe('HousingLocationComponent', () => {
   let component: HousingLocationComponent;
@@ -8,10 +9,21 @@ describe('HousingLocationComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HousingLocationComponent]
+      imports: [
+        HousingLocationComponent,
+        RouterTestingModule
+      ]
     });
     fixture = TestBed.createComponent(HousingLocationComponent);
     component = fixture.componentInstance;
+
+    component.housingLocation = {
+      id: 1,
+      name: 'Test Housing Location',
+      city: 'Test City',
+      state: 'TS',
+      photo: 'test.jpg'
+    } as HousingLocation;
     fixture.detectChanges();
   });
 
