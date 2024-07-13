19a5b57d49bb896c903e6173b20b067af2345632
rlinane <rlinane@gmail.com>
2024-07-13 14:38:38 -0400

    fix housing service spec

--- src/app/housing.service.spec.ts
+++ src/app/housing.service.spec.ts
@@ -13,4 +13,26 @@ describe('HousingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return all the housing locations', () => {
+    expect(service.getAllHousingLocations()).toBeTruthy();
+    const locations = service.getAllHousingLocations();
+    console.log(locations.then.length);
+    expect(locations.then.length).toBeGreaterThan(0);
+    service.submitApplication('John', 'Doe', 'john.doe@email.com');
+
+  });
+
+  it('should return a single housing location', () => {
+    expect(service.getHousingLocationById(1)).toBeTruthy();
+    let location = service.getHousingLocationById(1);
+    expect(location.then).toBeTruthy();
+    expect(location.then.length).toEqual(2);
+    console.log(location.then);
+    location = service.getHousingLocationById(-1);
+  });
+
+
+
+
 });
