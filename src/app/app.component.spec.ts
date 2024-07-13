0b5ecbce6afdc45501e7d986f68693073f6aea43
rlinane <rlinane@gmail.com>
2024-07-13 13:20:53 -0400

    fixed appComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,18 @@
-import { Component } from "@angular/core";
 import { TestBed, waitForAsync } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
 
-    let component: AppComponent;
-
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            declarations: [AppComponent],
-            imports: [RouterTestingModule]
+            imports: [AppComponent],
         }).compileComponents();
-        component = TestBed.inject(AppComponent);
     }));
 
     it('should be created', () => {
-        expect(component).toBeTruthy();
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
       });
     
 });
